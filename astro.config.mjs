c853de6a035502352d41c9137caf98c80b039724
Alejandro G. Lacasa <alejandro.lacasa@nexum.com>
2023-02-10 11:50:04 +0100

    Roll back to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,11 @@ import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-import cloudflare from '@astrojs/cloudflare'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [react(), tailwind()],
-  adapter: cloudflare(),
+  adapter: vercel(),
 })
