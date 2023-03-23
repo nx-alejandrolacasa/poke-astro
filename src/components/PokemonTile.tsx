ef931fb9819953c3aa45ed2c0216d28fc1b30861
Alejandro G. Lacasa <alejandro.lacasa@nexum.com>
2023-03-23 14:36:26 +0100

    Improve mobile view

--- src/components/PokemonTile.tsx
+++ src/components/PokemonTile.tsx
@@ -9,7 +9,7 @@ export function PokemonTile({ loading = false, pokemon }: PokemonTileProps) {
   return (
     <div className="text-center p-4 border border-white hover:border-slate-500 rounded-xl shadow-lg">
       <a href={`/pokemon/${pokemon.name}`} title={pokemon.name}>
-        <span className="block mb-4 font-bold text-xl capitalize text-ellipsis overflow-hidden whitespace-nowrap">
+        <span className="block mb-4 font-bold text-sm md:text-md xl:text-xl capitalize text-ellipsis overflow-hidden whitespace-nowrap">
           {pokemon.name.replaceAll('-', ' ')}
         </span>
         <img
